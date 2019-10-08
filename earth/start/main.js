878629f340512f0180f6de645223c04b40c85080
kateryna.pohrebniak <kateryna.pohrebniak@flaconi.de>
2019-10-08 14:39:02 +0200

    init

---
+++ earth/start/main.js
@@ -0,0 +1,13 @@
+const colors = ['#ff0000','#0000ff','#00ff00','#ffff00','#ff00ff','#ffffff'];
+
+function getColor(){
+    return colors[Math.floor(Math.random()*colors.length)];
+}
+
+
+window.onload = function() {
+    const aEntity = document.querySelector("#earthModel");
+    aEntity.addEventListener('click', function(ev, target){
+
+    });
+};
